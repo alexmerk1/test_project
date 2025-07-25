0122b5546fcb5ee79f5b9df1f19f215ab6a0abbc
alexmerk1 <alexmerk111@yandex.ru>
2025-07-25 06:28:38 +0300

    Create filters

--- test_project/items_app/admin.py
+++ test_project/items_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Item
 
-# Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('user', 'created_at')
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+admin.site.register(Item, ItemAdmin)
